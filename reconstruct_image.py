f38993d160eb0716afebe67be199b0d7f7455c28
Eitan Borgnia <eborgnia2@gmail.com>
2021-02-12 00:33:27 -0500

    Print statement to figure out the shape of the gradient vector used for reconstruction

--- reconstruct_image.py
+++ reconstruct_image.py
@@ -109,6 +109,7 @@ if __name__ == "__main__":
         input_gradient = [grad.detach() for grad in input_gradient]
         full_norm = torch.stack([g.norm() for g in input_gradient]).mean()
         print(f'Full gradient norm is {full_norm:e}.')
+        print('Gradient shape is: ', input_gradient.shape)
 
         # Run reconstruction in different precision?
         if args.dtype != 'float':
